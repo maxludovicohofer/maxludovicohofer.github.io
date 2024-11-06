c941ec77297e27c188fc6d1462bd0ef4b5a284c5
Max Hofer <hofermaxludovico@gmail.com>
2024-11-07 00:16:42 +0100

    feat: added blog post card UI and added spotlight to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import sentry from '@sentry/astro';
+import spotlightjs from '@spotlightjs/astro';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sentry(), spotlightjs()],
   site: 'https://maxludovicohofer.github.io',
 });
