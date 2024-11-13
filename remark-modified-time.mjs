271fbf82ebdec93e2174e40bf0d67f0517a9223f
Max Hofer <hofermaxludovico@gmail.com>
2024-11-13 12:36:08 +0100

    fix: fixing dayjs

--- remark-modified-time.mjs
+++ remark-modified-time.mjs
@@ -3,7 +3,9 @@ import { execSync } from "child_process";
 export function remarkModifiedTime() {
   return function (tree, file) {
     const filepath = file.history[0];
+    console.log(filepath);
     const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
+    console.log(result.toString());
     file.data.astro.frontmatter.lastModified = result.toString();
   };
 }
