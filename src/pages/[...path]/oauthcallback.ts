a344cda4614b714df609653d83f4653995f8a471
Max Hofer <hofermaxludovico@gmail.com>
2025-03-16 02:59:33 +0100

    feat: disable prerendering for oauthcallback and ensure proper authorization handling

--- src/pages/[...path]/oauthcallback.ts
+++ src/pages/[...path]/oauthcallback.ts
@@ -1,5 +1,7 @@
 import { completeAuthorization } from "@integrations/google";
 import type { APIRoute } from "astro";
 
+export const prerender = false;
+
 export const GET: APIRoute = async (Astro) =>
   await completeAuthorization(Astro);
