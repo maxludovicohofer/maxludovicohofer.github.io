2e7529009184042cf7e4746e067fa3511c49c5c7
Max Hofer <hofermaxludovico@gmail.com>
2025-03-19 15:47:44 +0100

    feat: add Sentry configuration for client and server

---
+++ sentry.client.config.ts
@@ -0,0 +1,6 @@
+import * as Sentry from "@sentry/astro";
+
+Sentry.init({
+  dsn: "https://5cccb7f2878ca1b593bab70b7d791312@o4508257933787136.ingest.de.sentry.io/4508257936736336",
+  integrations: [],
+});
